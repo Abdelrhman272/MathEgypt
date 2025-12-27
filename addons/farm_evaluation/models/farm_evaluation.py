683526a97880c7a0293b5687d7b339a2f1fd1b3f
Abdelrhman Elsayed <info@nextgensystems.net>
2025-12-28 00:40:19 +0200

    fix po

--- addons/farm_evaluation/models/farm_evaluation.py
+++ addons/farm_evaluation/models/farm_evaluation.py
@@ -98,7 +98,7 @@ class FarmEvaluation(models.Model):
                     "product_id": rec.raw_product_id.id,
                     "name": f"{rec.raw_product_id.display_name} - Grade {line.grade}",
                     "product_qty": line.expected_qty,
-                    "product_uom": rec.uom_id.id,
+                    "product_uom_id": rec.raw_product_id.uom_id.id,
                     "farm_grade": line.grade,
                     "farm_evaluation_line_id": line.id,
                 }))
