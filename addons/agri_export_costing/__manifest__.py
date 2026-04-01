f503b9a4d77380b87e216a630bfcbdd2da2b5c1c
Abdelrhman272 <abdelrhmanelsayed272@gmail.com>
2026-04-01 14:25:42 +0200

    Add shipment logistics costing and MRP relative sales value cost sharing

---
+++ addons/agri_export_costing/__manifest__.py
@@ -0,0 +1,17 @@
+# -*- coding: utf-8 -*-
+{
+    "name": "Agri Export Costing",
+    "version": "19.0.1.0.0",
+    "category": "Inventory",
+    "summary": "Shipment logistics costing and MRP relative sales value cost sharing",
+    "author": "NextGen Systems",
+    "license": "LGPL-3",
+    "depends": ["export_shipment", "agri_export_core", "sale_management", "mrp", "account", "mail"],
+    "data": [
+        "security/ir.model.access.csv",
+        "views/export_shipment_costing_views.xml",
+        "views/agri_production_costing_views.xml",
+    ],
+    "installable": True,
+    "application": False,
+}
